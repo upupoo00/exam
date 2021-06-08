9089c140077187cc6fc6ff259ed505571e32a9db
upupoo00 <upupoo0@163.com>
2021-06-08 17:59:46 +0800

    20210608每日一题

---
+++ src/Java0608/Main1.java
@@ -0,0 +1,43 @@
+package Java0608;
+
+/**
+ * 求和：https://www.nowcoder.com/questionTerminal/11cc498832db489786f8a03c3b67d02c
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main1{
+    static ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n, m;
+
+        while(sc.hasNext()) {
+            n = sc.nextInt();
+            m = sc.nextInt();
+            dfs(1, m, n);
+            for(ArrayList<Integer> l : res) {
+                int i = 0;
+                for(; i < l.size() - 1; i++) {
+                    System.out.print(l.get(i) + " ");
+                }
+                System.out.println(l.get(i));
+            }
+        }
+    }
+
+    public static void dfs(int index, int count, int n) {
+        if(count == 0) {
+            res.add(new ArrayList<>(list));
+        }
+        else {
+            for(int i = index; i <= count && i <= n; i++) {
+                list.add(i);
+                dfs(i + 1, count - i, n);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
