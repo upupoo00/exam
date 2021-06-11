530093d0b0955fcd090c11b8f0110276e92e77ba
upupoo00 <upupoo0@163.com>
2021-06-11 21:52:11 +0800

    20210611每日一题

--- src/Java0611/Solution.java
+++ src/Java0611/Solution.java
@@ -1,26 +1,27 @@
 package Java0611;
 
+import java.util.Arrays;
+
 /**
  * 数组中出现次数超过一半的数字:https://www.nowcoder.com/questionTerminal/e8a1b01a2df14cb2b228b30ee6a92163
  */
 public class Solution {
-    public int MoreThanHalfNum_Solution(int [] array) {
-        int count = 0;
+    public static int MoreThanHalfNum_Solution(int [] array) {
+        int count =array[0];
         int result = 0;
-        for(int i=0;i<array.length;i++){
-            count = 0;
-            for(int j=0;j<array.length;j++){
-
-                if (array[i]==array[j]){
-                    count++;
-                }
+        for(int num: array){
+            if(num == count) result++;
+            else result = result==0?0:result-1;
+            if(result==0){
+                count = num;
+                result = 1;
             }
-            if (count*2 >array.length){
-                result = array[i];
-            }
-
         }
-        return result;
+        return count;
+    }
 
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 2, 2, 2, 5, 4, 2};
+        System.out.println(MoreThanHalfNum_Solution(array));
     }
 }
